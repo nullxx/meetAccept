d85ab8ee643954564609577b8639f929fb3a9a0c
nullx <mail@nullx.me>
2020-04-28 12:17:35 +0200

    Working with spanish users

--- accept.js
+++ accept.js
@@ -2,8 +2,8 @@ window.setInterval(function() {
     var el = document.querySelectorAll('.g3VIld');
     if (el.length > 0) {
         for (var i = 0; i < el.length; i++) {
-            //if (el[i].getAttribute('aria-label') == 'Alguien quiere unirse a esta reunión') {
-            if (el[i].getAttribute('jsowner') == 'ow121') {
+            if (el[i].getAttribute('aria-label') == 'Alguien quiere unirse a esta reunión') {
+            //if (el[i].getAttribute('jsowner') == 'ow121') {
                 console.log("Aceptando...")
                 var denegar = el[i].children[3].children[0]
                 var aceptar = el[i].children[3].children[1]
